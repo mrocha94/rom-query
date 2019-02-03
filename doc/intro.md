877b6a49a11523041350d016417c730ae1b41225
Mateus Rocha <mmrocha94@gmail.com>
2019-02-02 23:01:24 -0200

    Init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rom-query
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rom_query/core_test.clj
@@ -0,0 +1,7 @@
+(ns rom-query.core-test
+  (:require [clojure.test :refer :all]
+            [rom-query.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
